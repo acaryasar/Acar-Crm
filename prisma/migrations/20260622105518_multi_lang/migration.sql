c91b50f7697ad3cb49ad6b65ca095b655c89e04a
Yaşar Acar <acaryasar@gmail.com>
2026-06-28 20:03:09 +0300

    .

--- prisma/migrations/20260622105518_multi_lang/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "public"."Locale" AS ENUM ('EN', 'DE', 'FR', 'RU', 'TR');
-
--- AlterTable
-ALTER TABLE "public"."User" ADD COLUMN     "locale" "public"."Locale" NOT NULL DEFAULT 'EN';
